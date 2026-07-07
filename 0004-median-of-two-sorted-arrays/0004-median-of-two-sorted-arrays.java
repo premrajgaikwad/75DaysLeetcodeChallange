4b0c5049e347786b0debc17f9cf61e01b5e6ef0e
premrajgaikwad <165922727+premrajgaikwad@users.noreply.github.com>
2026-07-07 13:57:49 +0530

    Time: 2 ms (47.59%), Space: 48.6 MB (86.81%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,54 +1,91 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
 
-        int m =nums1.length;
-        int n= nums2.length;
-        int[]merge=new int [m+n];
-        int i=0;
-        int j=0;
-        int k=0;
+int m =nums1.length;
+int n= nums2.length;
+ int [] merge=new int [m+n]; 
+ int i=0; int j=0; int k=0;
 
-        while (i<m && j< n){
+ while(i<m && j<n){
+   if(nums1[i]<nums2[j]){
+    merge[k]=nums1[i];
+    i++;
+   }else{
+    merge[k]=nums2[j];
+    j++;
+   }
+   k++;
+
+ }
+while(i<m){
+  merge[k]=nums1[i];
+  i++;
+   k++;
+
+
+}
+while(j<n){
+    merge[k]=nums2[j];
+    j++;
+    k++;
+
+}
+int total= merge.length;
+int mid=total/2;
+if (total %2 ==0){
+    return  (merge[mid-1] +merge[mid])/2.0;
+}
+else{
+    return (merge[mid]);
+}
+    }
+}
+
+
+//        int m =nums1.length;
+//         int n= nums2.length;
+//         int[]merge=new int [m+n];
+//         int i=0;
+//         int j=0;
+//         int k=0;
+
+//         while (i<m && j< n){
         
-        if (  nums1[i]< nums2[j]){
+//         if (  nums1[i]< nums2[j]){
             
-            merge[k]=nums1[i];
-            i++;
-        }
+//             merge[k]=nums1[i];
+//             i++;
+//         }
         
-        else{
+//         else{
            
-            merge[k]=nums2[j];
-            j++;
-        }
-        k++;
-    }
-    while(i<m){
+//             merge[k]=nums2[j];
+//             j++;
+//         }
+//         k++;
+//     }
+//     while(i<m){
        
-            merge[k]=nums1[i];
-            i++;
-            k++;
-    }
-    while (j<n){
+//             merge[k]=nums1[i];
+//             i++;
+//             k++;
+//     }
+//     while (j<n){
 
-            merge[k]=nums2[j];
-            j++;
-            k++;
-    }
-int total = merge.length;
-int mid=total /2;
+//             merge[k]=nums2[j];
+//             j++;
+//             k++;
+//     }
+// int total = merge.length;
+// int mid=total /2;
 
-if ( total % 2 ==0){
-  return (merge[mid-1] + merge[mid])/2.0;
-}
-else{ 
-    return merge[mid];
-}
-
-
+// if ( total % 2 ==0){
+//   return (merge[mid-1] + merge[mid])/2.0;
+// }
+// else{ 
+//     return merge[mid];
+// }
 
 
-        
 
-    }
-}
+
